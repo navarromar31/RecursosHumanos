e88298fef79b0c30ebb96beeeb90f413e21ea5f8
Chale29 <125849321+Chale29@users.noreply.github.com>
2024-09-28 10:54:03 -0600

    Controladores PUESTO y Colaborador
    estan casi completos, falta el atributo imagen e imagen en el controlador de colaborador

---
+++ RecursosHumanos/Controllers/ColaboradorController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecursosHumanos.Datos;
+using WebApplication1.Models;
+
+namespace RecursosHumanos.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+
+        private readonly AplicationDbContext _db;
+
+        public ColaboradorController(AplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Colaborador> lista = _db.colaborador;
+
+            return View(lista);
+        }
+
+        //CREAR
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.colaborador.Add(colaborador);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(colaborador);
+        }
+
+        //EDITAR
+
+        public IActionResult Editar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+
+            }
+
+            var obj = _db.colaborador.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.colaborador.Update(colaborador);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(colaborador);
+        }
+
+        //ELIMINAR
+
+        public IActionResult Eliminar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+
+            }
+
+            var obj = _db.colaborador.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Eliminar(Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            _db.colaborador.Remove(colaborador);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ RecursosHumanos/Controllers/PuestoController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecursosHumanos.Datos;
+using WebApplication1.Models;
+
+namespace RecursosHumanos.Controllers
+{
+    public class PuestoController : Controller
+    {
+
+        private readonly AplicationDbContext _db;
+
+        public PuestoController(AplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Puesto> lista = _db.puesto;
+
+            return View(lista);
+        }
+
+        //CREAR
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Puesto puesto)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.puesto.Add(puesto);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(puesto);
+        }
+
+        //EDITAR
+
+        public IActionResult Editar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+
+            }
+
+            var obj = _db.puesto.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Puesto puesto)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.puesto.Update(puesto);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(puesto);
+        }
+
+        //ELIMINAR
+
+        public IActionResult Eliminar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+
+            }
+
+            var obj = _db.puesto.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Eliminar(Puesto puesto)
+        {
+            if (ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            _db.puesto.Remove(puesto);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
