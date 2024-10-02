9ddc3b8603da95ac3c91ea234e690936724a6dfc
Nayleaa123 <ariasnaylea28@gmail.com>
2024-10-01 22:58:01 -0600

    agregar eliminar el los controller de departamento e institucion
    listo

--- RecursosHumanos/Controllers/DepartamentoController.cs
+++ RecursosHumanos/Controllers/DepartamentoController.cs
@@ -1,109 +1,172 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using RecursosHumanos.Datos;
+using RecursosHumanos.Models;
+using RecursosHumanos.Models.ViewModels;
+using System.Collections.Generic;
 using RecursosHumanos.Models;
 
 namespace RecursosHumanos.Controllers
-
 {
     public class DepartamentoController : Controller
     {
 
         private readonly AplicationDbContext _db;
-
-        public DepartamentoController(AplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public DepartamentoController(AplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
-
 
         public IActionResult Index()
         {
-            IEnumerable<Departamento> lista = _db.departamento;
+            IEnumerable<Departamento> lista = _db.departamento
+                .Include(c => c.Departamento);
 
             return View(lista);
         }
 
-        //Get
-
-        public IActionResult Crear()
+        public IActionResult Upsert(int? Id)
         {
-            return View();
+            DepartamentoVM departamentoVM = new DepartamentoVM()
+            {
+                Departamento = new Departamento(),
+                DepartamentoLista = _db.departamento.Select(c => new SelectListItem
+                {
+                    Text = c.NombreDepartamento,
+                    Value = c.Id.ToString()
+                }),
+
+            };
+
+            if (Id == null)
+            {
+                return View(departamentoVM);
+            }
+            else
+            {
+                departamentoVM.Departamento = _db.departamento.Find(Id);
+                if (departamentoVM == null)
+                {
+                    return NotFound();
+                }
+                return View(departamentoVM);
+            }
+
         }
 
-        //post
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Crear(Departamento departamento)
+        public IActionResult Upsert(DepartamentoVM departamentoVM)
         {
             if (ModelState.IsValid)
             {
-                _db.departamento.Add(departamento);
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                if (departamentoVM.Departamento.Id == 0)
+                {
+                    string upload = webRootPath + WC.ImagenRuta;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    //guardar imagen
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    departamentoVM.Departamento.ImagenUrlCap = fileName + extension;
+                    _db.departamento.Add(departamentoVM.Departamento);
+                }
+                else
+                {
+
+
+                    var objDepartamento = _db.departamento.AsNoTracking().FirstOrDefault(p => p.Id == departamentoVM.Departamento.Id);
+
+
+
+                    if (files.Count > 0)
+                    {
+                        string upload = webRootPath + WC.ImagenRuta;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        var anteriorFile = Path.Combine(upload, objDepartamento.ImagenUrlCap + extension);
+
+
+                        if (System.IO.File.Exists(anteriorFile))
+                        {
+                            System.IO.File.Delete(anteriorFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+
+                        departamentoVM.DepartamentoImagenUrlCap = fileName + extension;
+                    }
+                    else
+                    {
+                        departamentoVM.Departamento.ImagenUrlCap = objDepartamento.ImagenUrlCap;
+                    }
+
+                    _db.departamento.Update(departamentoVM.Departamento);
+                }
+
                 _db.SaveChanges();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index");
 
             }
-            return View(departamento);
+
+            return View(departamentoVM);
+
         }
 
-        public IActionResult Editar(int? Id)
-        {
-            if (Id == null || Id == 0)
-            {
-                return NotFound();
-
-            }
-
-            var obj = _db.departamento.Find(Id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
-        }
-
-        //post
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Editar(Departamento departamento)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.departamento.Update(departamento);
-                _db.SaveChanges();
-                return RedirectToAction(nameof(Index));
-
-            }
-            return View(departamento);
-        }
-
+        //get
         public IActionResult Eliminar(int? Id)
         {
             if (Id == null || Id == 0)
+            { return NotFound(); }
+
+            Departamento departamento = _db.departamento.Include(c => c.NombreDepartamento)
+                .FirstOrDefault(p => p.Id == Id);
+
+            if (departamento == null)
             {
                 return NotFound();
-
             }
-
-            var obj = _db.departamento.Find(Id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
+            return View(departamento);
         }
 
-        //post
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Eliminar(Departamento departamento)
         {
-            if (ModelState.IsValid)
+            if (departamento == null)
             {
                 return NotFound();
             }
-            _db.departamento.Remove(departamento);
+
+            string upload = _webHostEnvironment.WebRootPath + WC.ImagenRuta;
+            var anteriorFile = Path.Combine(upload, departamento.ImagenUrlCap);
+
+
+            if (System.IO.File.Exists(anteriorFile))
+            {
+                System.IO.File.Delete(anteriorFile);
+            }
+            _db.departamento.RemoveRange(departamento);
             _db.SaveChanges();
-            return RedirectToAction(nameof(Index));
+
+            return RedirectToAction("Index");
         }
+
     }
 }
--- RecursosHumanos/Controllers/InstitucionController.cs
+++ RecursosHumanos/Controllers/InstitucionController.cs
@@ -1,109 +1,171 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using RecursosHumanos.Datos;
+using RecursosHumanos.Models;
+using RecursosHumanos.Models.ViewModels;
+using System.Collections.Generic;
 using RecursosHumanos.Models;
 
 namespace RecursosHumanos.Controllers
-
 {
-    public class IntitucionController : Controller
+    public class InstitucionController : Controller
     {
 
         private readonly AplicationDbContext _db;
-
-        public IntitucionController(AplicationDbContext db)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public InstitucionController(AplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
+            _webHostEnvironment = webHostEnvironment;
         }
-
 
         public IActionResult Index()
         {
-            IEnumerable<Institucion> lista = _db.institucion;
+            IEnumerable<Institucion> lista = _db.institucion
+                .Include(c => c.Institucion);
 
             return View(lista);
         }
 
-        //Get
-
-        public IActionResult Crear()
+        public IActionResult Upsert(int? Id)
         {
-            return View();
+            InstitucionVM institucionVM = new InstitucionVM()
+            {
+                Institucion = new Institucion(),
+                InstitucionLista = _db.institucion.Select(c => new SelectListItem
+                {
+                    Text = c.NombreInstitucion,
+                    Value = c.Id.ToString()
+                }),
+
+            };
+
+            if (Id == null)
+            {
+                return View(institucionVM);
+            }
+            else
+            {
+                institucionVM.Institucion = _db.institucion.Find(Id);
+                if (institucionVM == null)
+                {
+                    return NotFound();
+                }
+                return View(institucionVM);
+            }
         }
 
-        //post
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Crear(Institucion institucion)
+        public IActionResult Upsert(InstitucionVM institucionVM)
         {
             if (ModelState.IsValid)
             {
-                _db.institucion.Add(institucion);
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                if (institucionVM.Institucion.Id == 0)
+                {
+                    string upload = webRootPath + WC.ImagenRuta;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    //guardar imagen
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    institucionVM.Institucion.ImagenUrlCap = fileName + extension;
+                    _db.institucion.Add(institucionVM.Institucion);
+                }
+                else
+                {
+
+
+                    var objInstitucion = _db.institucion.AsNoTracking().FirstOrDefault(p => p.Id == institucionVM.Institucion.Id);
+
+
+
+                    if (files.Count > 0)
+                    {
+                        string upload = webRootPath + WC.ImagenRuta;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        var anteriorFile = Path.Combine(upload, objInstitucion.ImagenUrlCap + extension);
+
+
+                        if (System.IO.File.Exists(anteriorFile))
+                        {
+                            System.IO.File.Delete(anteriorFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+
+                        institucionVM.InstitucionImagenUrlCap = fileName + extension;
+                    }
+                    else
+                    {
+                        institucionVM.Institucion.ImagenUrlCap = objInstitucion.ImagenUrlCap;
+                    }
+
+                    _db.institucion.Update(institucionVM.Institucion);
+                }
+
                 _db.SaveChanges();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index");
 
             }
-            return View(institucion);
+
+            return View(institucionVM);
+
         }
 
-        public IActionResult Editar(int? Id)
-        {
-            if (Id == null || Id == 0)
-            {
-                return NotFound();
-
-            }
-
-            var obj = _db.institucion.Find(Id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
-        }
-
-        //post
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Editar(Institucion institucion)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.institucion.Update(institucion);
-                _db.SaveChanges();
-                return RedirectToAction(nameof(Index));
-
-            }
-            return View(institucion);
-        }
-
+        //get
         public IActionResult Eliminar(int? Id)
         {
             if (Id == null || Id == 0)
+            { return NotFound(); }
+
+            Institucion institucion = _db.institucion.Include(c => c.NombreInstitucion)
+                .FirstOrDefault(p => p.Id == Id);
+
+            if (institucion == null)
             {
                 return NotFound();
-
             }
-
-            var obj = _db.institucion.Find(Id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            return View(obj);
+            return View(institucion);
         }
 
-        //post
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Eliminar(Institucion institucion)
         {
-            if (ModelState.IsValid)
+            if (institucion == null)
             {
                 return NotFound();
             }
-            _db.institucion.Remove(institucion);
+
+            string upload = _webHostEnvironment.WebRootPath + WC.ImagenRuta;
+            var anteriorFile = Path.Combine(upload, institucion.ImagenUrlCap);
+
+
+            if (System.IO.File.Exists(anteriorFile))
+            {
+                System.IO.File.Delete(anteriorFile);
+            }
+            _db.institucion.RemoveRange(institucion);
             _db.SaveChanges();
-            return RedirectToAction(nameof(Index));
+
+            return RedirectToAction("Index");
         }
+
     }
 }
