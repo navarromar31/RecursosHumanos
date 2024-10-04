317a66e335c7369b0594797cfd5c9869aa720373
Chale29 <125849321+Chale29@users.noreply.github.com>
2024-10-03 20:51:26 -0600

    guarde

--- RecursosHumanos/obj/Debug/net6.0/RecursosHumanos.AssemblyInfo.cs
+++ RecursosHumanos/obj/Debug/net6.0/RecursosHumanos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddc3b8603da95ac3c91ea234e690936724a6dfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13113129cffac04cad5f0112c8367d0f53e03e29")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
