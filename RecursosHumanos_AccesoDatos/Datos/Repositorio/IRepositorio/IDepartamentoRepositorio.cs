3fa84f300269f3c3cde1bf713e2cf4119f1685cf
Nayleaa123 <ariasnaylea28@gmail.com>
2024-11-04 21:15:03 -0600

    repositorios
    interfaces

---
+++ RecursosHumanos_AccesoDatos/Datos/Repositorio/IRepositorio/IDepartamentoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using RecursosHumanos_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursosHumanos_AccesoDatos.Datos.Repositorio.IRepositorio
+{
+    public interface IDepartamentoRepositorio : IRepositorio<Departamento>
+    {
+        void Actualizar(Departamento departamento);
+
+
+    }
+}
---
+++ RecursosHumanos_AccesoDatos/Datos/Repositorio/IRepositorio/IPuestoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using RecursosHumanos_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursosHumanos_AccesoDatos.Datos.Repositorio.IRepositorio
+{
+    public interface IPuestoRepositorio : IRepositorio<Puesto>
+    {
+        void Actualizar(Puesto puesto);
+
+    }
+}
