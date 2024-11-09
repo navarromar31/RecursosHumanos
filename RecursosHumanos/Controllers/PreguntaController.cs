154caeab5ee82f0263603728135d4eb483a12cc4
dayra-15 <dayralizsolano@gmail.com>
2024-11-09 10:05:13 -0600

    me metí a revisar

--- RecursosHumanos/Controllers/PreguntaController.cs
+++ RecursosHumanos/Controllers/PreguntaController.cs
@@ -86,77 +86,7 @@ namespace RecursosHumanos.Controllers
         [HttpPost]
         public IActionResult Upsert(PreguntaVM preguntaVM)
         {
-            //validamos el modelo
-            /*if (ModelState.IsValid)
-            {
-                //esto para poder obtener la imagen que nos envia la vista
-                var files = HttpContext.Request.Form.Files;
-                string webRootPath = _webHostEnvironment.WebRootPath;
-                //ahora validamos si es o no un nuevo ingreso o si se trata de una actualizacion 
-                if (colaboradorVM.Colaborador.Id == 0)
-                {
-                    //toda esta logica es para lograr grabar un colaborador y una imagen
-                    //crear si el ID es cero
-                    string upload = webRootPath + WC.ImagenRuta;//LA PRIPIEDAD DE WC ES LA QUE TIENE LA RUTA DE DONDE GUARDAR LA IMAGEN
-                    string fileName = Guid.NewGuid().ToString(); //esto es para que se le asigne un ID a la iamgen que se va a guardar
-                    string extension = Path.GetExtension(files[0].FileName);
-
-                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
-                    {
-                        files[0].CopyTo(fileStream);
-                    }
-                    //finalmente grabanos la imagen y los datos del colaborador en los campos correspondientes
-
-                    colaboradorVM.Colaborador.ImagenUrlCol = fileName + extension;
-                    _colaboradorRepo.Agregar(colaboradorVM.Colaborador);
-                }
-                else
-                {
-                    //se actualiza si el ID es mayor a cero
-                    var objColaborador = _colaboradorRepo.ObtenerPrimero(p => p.Id == colaboradorVM.Colaborador.Id, isTracking: false);   //Obtenemos el producto que queremos editar 
-
-                    if (files.Count > 0)//validamos con este si existe una imagen y si es asi el usuario lo que va hacer es cambiar la imagen actual por otra 
-                    {
-                        string upload = webRootPath + WC.ImagenRuta;//LA PRIPIEDAD DE WC ES LA QUE TIENE LA RUTA DE DONDE GUARDAR LA IMAGEN
-                        string fileName = Guid.NewGuid().ToString(); //esto es para que se le asigne un ID a la iamgen que se va a guardar
-                        string extension = Path.GetExtension(files[0].FileName);
-                        //pero tendriamos que borrar la img anterior
-
-                        var anteriorFile = Path.Combine(upload, objColaborador.ImagenUrlCol); //con esto obtnemos la anterior
-                        if (System.IO.File.Exists(anteriorFile))//VERIFICAMOS SI LA IMG ANTERIOR EXISTE
-                        {
-                            System.IO.File.Delete(anteriorFile);    // SI EXISTE LA BORRAMOS
-                        }  //FIN DE BORRAR LA IMG ANTERIOR                                                        //
-
-                        //PROCEDEMOS A CARGAR LA NUEVA IMAGEN
-                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
-                        {
-                            files[0].CopyTo(fileStream);
-                        }
-
-                        //PROCEDEMOS ACTUALIZAR EL PRODUCTO CON SU NUEVA IMG
-
-                        colaboradorVM.Colaborador.ImagenUrlCol = fileName + extension;
-
-                    }//CASO CONTRARIO SI NO SE CARGA UNA NUEVA IMG , ES DECIR ACTUALIZA OTROS DATOS PERO LA IMG NO
-                    else
-                    {
-                        //en ese caso se mantiene la misma img
-
-                        colaboradorVM.Colaborador.ImagenUrlCol = objColaborador.ImagenUrlCol;
-
-                    }
-                    //ya luego actualizamos el producto
-
-                    _colaboradorRepo.Actualizar(colaboradorVM.Colaborador);
-
-                }
-
-                _colaboradorRepo.Grabar();
-                return RedirectToAction("Index");
-            }//ESTA LLAVE PERTENCE AL IF DEL ModelIsValidate
-            */
-
+  
             preguntaVM.EvaluacionLista = _preguntaRepo.ObtenerTodosDropDownList(WC.EvaluacionNombre);
             preguntaVM.CapacitacionLista = _preguntaRepo.ObtenerTodosDropDownList(WC.CapacitacionNombre);
 
@@ -164,7 +94,6 @@ namespace RecursosHumanos.Controllers
 
         }
 
-        // ACA NO SOLAMENTE ELIMINAMOS EL PRODUCTO , SINO TBM LA IMG ASOCIADA A ESTE
         //GET
 
 
@@ -201,18 +130,6 @@ namespace RecursosHumanos.Controllers
                 return NotFound();
 
             }
-
-            //ahora lo primero es proceder a eliminar la imagen de nuestro server 
-
-
-            //string upload = _webHostEnvironment.WebRootPath + WC.ImagenRuta;//LA PRoPIEDAD DE WC ES LA QUE TIENE LA RUTA DE DONDE esta  GUARDAda LA IMAGEN
-
-
-            /*var anteriorFile = Path.Combine(upload, colaborador.ImagenUrlCol);
-            if (System.IO.File.Exists(anteriorFile))//VERIFICAMOS SI LA IMG ANTERIOR EXISTE
-            {
-                System.IO.File.Delete(anteriorFile);    // SI EXISTE LA BORRAMOS
-            }*/
 
             _preguntaRepo.Remover(pregunta);  //Ahora eliminamos el producto
             _preguntaRepo.Grabar();
--- RecursosHumanos/Controllers/RespuestaController.cs
+++ RecursosHumanos/Controllers/RespuestaController.cs
@@ -85,76 +85,6 @@ namespace RecursosHumanos.Controllers
         [HttpPost]
         public IActionResult Upsert(RespuestaVM respuestaVM)
         {
-            //validamos el modelo
-            /*if (ModelState.IsValid)
-            {
-                //esto para poder obtener la imagen que nos envia la vista
-                var files = HttpContext.Request.Form.Files;
-                string webRootPath = _webHostEnvironment.WebRootPath;
-                //ahora validamos si es o no un nuevo ingreso o si se trata de una actualizacion 
-                if (colaboradorVM.Colaborador.Id == 0)
-                {
-                    //toda esta logica es para lograr grabar un colaborador y una imagen
-                    //crear si el ID es cero
-                    string upload = webRootPath + WC.ImagenRuta;//LA PRIPIEDAD DE WC ES LA QUE TIENE LA RUTA DE DONDE GUARDAR LA IMAGEN
-                    string fileName = Guid.NewGuid().ToString(); //esto es para que se le asigne un ID a la iamgen que se va a guardar
-                    string extension = Path.GetExtension(files[0].FileName);
-
-                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
-                    {
-                        files[0].CopyTo(fileStream);
-                    }
-                    //finalmente grabanos la imagen y los datos del colaborador en los campos correspondientes
-
-                    colaboradorVM.Colaborador.ImagenUrlCol = fileName + extension;
-                    _colaboradorRepo.Agregar(colaboradorVM.Colaborador);
-                }
-                else
-                {
-                    //se actualiza si el ID es mayor a cero
-                    var objColaborador = _colaboradorRepo.ObtenerPrimero(p => p.Id == colaboradorVM.Colaborador.Id, isTracking: false);   //Obtenemos el producto que queremos editar 
-
-                    if (files.Count > 0)//validamos con este si existe una imagen y si es asi el usuario lo que va hacer es cambiar la imagen actual por otra 
-                    {
-                        string upload = webRootPath + WC.ImagenRuta;//LA PRIPIEDAD DE WC ES LA QUE TIENE LA RUTA DE DONDE GUARDAR LA IMAGEN
-                        string fileName = Guid.NewGuid().ToString(); //esto es para que se le asigne un ID a la iamgen que se va a guardar
-                        string extension = Path.GetExtension(files[0].FileName);
-                        //pero tendriamos que borrar la img anterior
-
-                        var anteriorFile = Path.Combine(upload, objColaborador.ImagenUrlCol); //con esto obtnemos la anterior
-                        if (System.IO.File.Exists(anteriorFile))//VERIFICAMOS SI LA IMG ANTERIOR EXISTE
-                        {
-                            System.IO.File.Delete(anteriorFile);    // SI EXISTE LA BORRAMOS
-                        }  //FIN DE BORRAR LA IMG ANTERIOR                                                        //
-
-                        //PROCEDEMOS A CARGAR LA NUEVA IMAGEN
-                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
-                        {
-                            files[0].CopyTo(fileStream);
-                        }
-
-                        //PROCEDEMOS ACTUALIZAR EL PRODUCTO CON SU NUEVA IMG
-
-                        colaboradorVM.Colaborador.ImagenUrlCol = fileName + extension;
-
-                    }//CASO CONTRARIO SI NO SE CARGA UNA NUEVA IMG , ES DECIR ACTUALIZA OTROS DATOS PERO LA IMG NO
-                    else
-                    {
-                        //en ese caso se mantiene la misma img
-
-                        colaboradorVM.Colaborador.ImagenUrlCol = objColaborador.ImagenUrlCol;
-
-                    }
-                    //ya luego actualizamos el producto
-
-                    _colaboradorRepo.Actualizar(colaboradorVM.Colaborador);
-
-                }
-
-                _colaboradorRepo.Grabar();
-                return RedirectToAction("Index");
-            }//ESTA LLAVE PERTENCE AL IF DEL ModelIsValidate
-            */
 
             respuestaVM.ColaboradorLista = _respuestaRepo.ObtenerTodosDropDownList(WC.ColaboradorNombre);
             respuestaVM.PreguntasLista = _respuestaRepo.ObtenerTodosDropDownList(WC.PreguntaNo);
@@ -200,18 +130,6 @@ namespace RecursosHumanos.Controllers
                 return NotFound();
 
             }
-
-            //ahora lo primero es proceder a eliminar la imagen de nuestro server 
-
-
-            //string upload = _webHostEnvironment.WebRootPath + WC.ImagenRuta;//LA PRoPIEDAD DE WC ES LA QUE TIENE LA RUTA DE DONDE esta  GUARDAda LA IMAGEN
-
-
-            /*var anteriorFile = Path.Combine(upload, colaborador.ImagenUrlCol);
-            if (System.IO.File.Exists(anteriorFile))//VERIFICAMOS SI LA IMG ANTERIOR EXISTE
-            {
-                System.IO.File.Delete(anteriorFile);    // SI EXISTE LA BORRAMOS
-            }*/
 
             _respuestaRepo.Remover(respuesta);  //Ahora eliminamos el producto
             _respuestaRepo.Grabar();
