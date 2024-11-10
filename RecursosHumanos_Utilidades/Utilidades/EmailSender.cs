485be09098403b2c441205d0e5b763f14ce2849d
sarilu15 <sara.sarmiento.gutierrez@covao.ed.cr>
2024-11-10 17:37:06 -0600

    Sendgrid subite o lloro

---
+++ RecursosHumanos_Utilidades/Utilidades/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ferretero_Utilidades
+{
+    public class EmailSender : IEmailSender
+    {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config) //el IConfiguration nos permitira acceder al AppSetting.Json
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");//asi capturamos la APIKey
+        }
+
+        //aca creo la estructura de email y finalmente lo envio
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("sara.sarmiento.gutierrez@covao.ed.cr");
+            var to = new EmailAddress(email);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
---
+++ RecursosHumanos_Utilidades/Utilidades/SessionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace Ferretero_Utilidades
+{
+    public static class SessionExtensions
+    {
+        // Método de extensión para establecer un valor en la sesión
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            // Serializa el valor a JSON y almacénalo en la sesión
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        // Método de extensión para obtener un valor de la sesión
+        public static T Get<T>(this ISession session, string key)
+        {
+            // Recupera el valor serializado de la sesión
+            var value = session.GetString(key);
+
+            // Deserializa el valor desde JSON y devuélvelo
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+        }
+    }
+}
