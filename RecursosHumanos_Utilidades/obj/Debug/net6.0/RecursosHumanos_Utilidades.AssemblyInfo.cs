087c7440d02ccf2e4302a0e83abcd44481c94bcc
dayra-15 <dayralizsolano@gmail.com>
2024-11-21 21:50:38 -0600

    me meti

--- RecursosHumanos_Utilidades/obj/Debug/net6.0/RecursosHumanos_Utilidades.AssemblyInfo.cs
+++ RecursosHumanos_Utilidades/obj/Debug/net6.0/RecursosHumanos_Utilidades.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecursosHumanos_Utilidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b5b89757cfd58e668d6e2b250a300238eb03f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17bb43a5717e4097dd1fb804d450289a84220d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecursosHumanos_Utilidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecursosHumanos_Utilidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
