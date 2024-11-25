79833b06e32a6a1bbfb0ed2fc571fd4134250be9
Chale29 <125849321+Chale29@users.noreply.github.com>
2024-11-25 14:56:09 -0600

    Ya no hay mas cambios x hacer
    YA

--- RecursosHumanos/obj/Debug/net6.0/RecursosHumanos.AssemblyInfo.cs
+++ RecursosHumanos/obj/Debug/net6.0/RecursosHumanos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7250512b1dd3fe7032b5d5aa3431f057944d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e962a8336c03862d204f3e12aa5094f17cb50b9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
