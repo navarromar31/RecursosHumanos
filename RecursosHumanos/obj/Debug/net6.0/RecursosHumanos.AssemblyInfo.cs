72fe49f648a366fe4a9aef8e315543f011e2cd57
dayra-15 <dayralizsolano@gmail.com>
2024-11-26 20:31:40 -0600

    cambios

--- RecursosHumanos/obj/Debug/net6.0/RecursosHumanos.AssemblyInfo.cs
+++ RecursosHumanos/obj/Debug/net6.0/RecursosHumanos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89047738c301b9a9f5a49ec85073a24c115e17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68217cddf637795335aef47569a9d5e5b53b64a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecursosHumanos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RecursosHumanos_Models/obj/Debug/net6.0/RecursosHumanos_Models.AssemblyInfo.cs
+++ RecursosHumanos_Models/obj/Debug/net6.0/RecursosHumanos_Models.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecursosHumanos_Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89047738c301b9a9f5a49ec85073a24c115e17e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68217cddf637795335aef47569a9d5e5b53b64a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecursosHumanos_Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecursosHumanos_Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
